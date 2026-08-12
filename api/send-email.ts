140fefd538d884da830d61c7d4d721cc56355556
attayussif <atta5yussif@gmail.com>
2026-08-12 21:12:05 +0000

    Update send-email.ts

--- api/send-email.ts
+++ api/send-email.ts
@@ -1,5 +1,10 @@
-export default function handler(req, res) {
-  res.status(200).json({
+import type { VercelRequest, VercelResponse } from "@vercel/node";
+
+export default function handler(
+  req: VercelRequest,
+  res: VercelResponse,
+) {
+  return res.status(200).json({
     ok: true,
     message: "Vercel Function works",
   });
